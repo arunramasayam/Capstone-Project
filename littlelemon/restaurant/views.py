ff195cac7ffb98c5999f9479e09e72181a0ad77a
arunramasayam <ramasayamarun5@gmail.com>
2024-03-13 16:59:33 +0530

    Adding Booingviewset class and serializers for crud operaions and also routers in url patterns to wire booking url and bookingviewset class

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
-from .models import Menu
+from .models import Menu, Booking
 from django.http import HttpResponse
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from .serializers import MenuSerializer
+from rest_framework import generics, viewsets
+from .serializers import MenuSerializer, BookingSerializer
 # Create your views here.
 
 def index(request):
     return render(request, "index.html", {})
 
 
-class MenuItemsView(ListCreateAPIView):
+class MenuItemsView(generics.ListCreateAPIView):
     queryset=Menu.objects.all()
     serializer_class=MenuSerializer
 
 
 
-class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset=Menu.objects.all()
     serializer_class=MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
+
